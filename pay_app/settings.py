3b449861004d59a690f76d2adb219161e1d27b11
strongestavenger <mikemutoro@gmail.com>
2020-01-14 12:07:43 +0300

    edit settings

--- pay_app/settings.py
+++ pay_app/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
     'localhost',
-    'https://mpay-app.herokuapp.com/'
+    'https://mpay-app.herokuapp.com'
 ]
 
 
--- pay_app/urls.py
+++ pay_app/urls.py
@@ -5,6 +5,6 @@ from pay_app.apps.api_calls.views import PaymentC2BView, ConfirmationC2BView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/validation", PaymentC2BView.as_view(), name="validation"),
-    path("api/v1/confirmation", ConfirmationC2BView.as_view(), name="confirmation")
+    path("api/v1/validation/", PaymentC2BView.as_view(), name="validation"),
+    path("api/v1/confirmation/", ConfirmationC2BView.as_view(), name="confirmation")
 ]
